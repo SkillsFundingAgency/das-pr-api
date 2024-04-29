7e5ec804e94e3d05e7797256642c359634760bb3
shaun-downey-education <Shaun.Downey@education.gov.uk>
2024-04-29 11:28:42 +0100

    CSP-1315: Updated GetAccountProvidersQueryResult to record.

--- src/SFA.DAS.PR.Application/AccountProviders/Queries/GetAccountProviders/GetAccountProvidersQueryResult.cs
+++ src/SFA.DAS.PR.Application/AccountProviders/Queries/GetAccountProviders/GetAccountProvidersQueryResult.cs
@@ -1,7 +1,3 @@
 ﻿namespace SFA.DAS.PR.Application.AccountProviders.Queries.GetAccountProviders;
 
-public class GetAccountProvidersQueryResult(long accountId, List<AccountProviderModel> accountProviders)
-{
-    public long AccountId { get; set; } = accountId;
-    public List<AccountProviderModel> AccountProviders { get; set; } = accountProviders;
-}
+public record GetAccountProvidersQueryResult(long AccountId, List<AccountProviderModel> AccountProviders);
