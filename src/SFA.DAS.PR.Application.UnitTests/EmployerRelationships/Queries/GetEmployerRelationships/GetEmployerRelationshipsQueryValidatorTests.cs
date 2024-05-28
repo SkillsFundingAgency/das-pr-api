0e6e713c5ebc642a013a626e2cf2a1714e57fb88
shaun-downey-education <Shaun.Downey@education.gov.uk>
2024-05-28 16:30:08 +0100

    CSP-1410: Conditional navigation between Ukprn and account legal entity public hashed id.

--- src/SFA.DAS.PR.Application.UnitTests/EmployerRelationships/Queries/GetEmployerRelationships/GetEmployerRelationshipsQueryValidatorTests.cs
+++ src/SFA.DAS.PR.Application.UnitTests/EmployerRelationships/Queries/GetEmployerRelationships/GetEmployerRelationshipsQueryValidatorTests.cs
@@ -34,8 +34,35 @@ public class GetEmployerRelationshipsQueryValidatorTests
     public async Task ValidateUkprn_Invalid()
     {
         var sut = new GetEmployerRelationshipsQueryValidator();
-        var result = await sut.TestValidateAsync(new GetEmployerRelationshipsQuery("hash", 1001, null));
+        var result = await sut.TestValidateAsync(new GetEmployerRelationshipsQuery("hash", 1001, "hash"));
         result.ShouldHaveValidationErrorFor(q => q.Ukprn)
                     .WithErrorMessage(UkprnFormatValidator.UkprnFormatValidationMessage);
+    }
+
+    [Test]
+    public async Task ValidateConditionaProperties_Missing_PublicHashedId_Invalid()
+    {
+        var sut = new GetEmployerRelationshipsQueryValidator();
+        var result = await sut.TestValidateAsync(new GetEmployerRelationshipsQuery("hash", 10000001, null));
+        result.ShouldHaveValidationErrorFor(q => q.AccountlegalentityPublicHashedId)
+                    .WithErrorMessage(GetEmployerRelationshipsQueryValidator.ConditionalParamsValidationMessage);
+    }
+
+    [Test]
+    public async Task ValidateConditionaProperties_Missing_Ukprn_Invalid()
+    {
+        var sut = new GetEmployerRelationshipsQueryValidator();
+        var result = await sut.TestValidateAsync(new GetEmployerRelationshipsQuery("hash", null, "hash"));
+        result.ShouldHaveValidationErrorFor(q => q.Ukprn)
+                    .WithErrorMessage(GetEmployerRelationshipsQueryValidator.ConditionalParamsValidationMessage);
+    }
+
+    [Test]
+    public async Task ValidateConditionaProperties_Valid_Parameters()
+    {
+        var sut = new GetEmployerRelationshipsQueryValidator();
+        var result = await sut.TestValidateAsync(new GetEmployerRelationshipsQuery("hash", 10000003, "hash"));
+        result.ShouldNotHaveValidationErrorFor(query => query.Ukprn);
+        result.ShouldNotHaveValidationErrorFor(query => query.AccountlegalentityPublicHashedId);
     }
 }
--- src/SFA.DAS.PR.Application/EmployerRelationships/Queries/GetProviderRelationships/GetEmployerRelationshipsQueryValidator.cs
+++ src/SFA.DAS.PR.Application/EmployerRelationships/Queries/GetProviderRelationships/GetEmployerRelationshipsQueryValidator.cs
@@ -5,15 +5,27 @@ namespace SFA.DAS.PR.Application.Permissions.Queries.GetEmployerRelationships;
 public class GetEmployerRelationshipsQueryValidator : AbstractValidator<GetEmployerRelationshipsQuery>
 {
     public const string AccountHashedIdValidationMessage = "An AccountHashedId must be supplied.";
+    public const string ConditionalParamsValidationMessage = "If one of Ukprn or AccountlegalentityPublicHashedId has a value then both properties must be populated.";
+    
     public GetEmployerRelationshipsQueryValidator()
     {
         RuleFor(a => a.AccountHashedId)
             .NotEmpty()
             .WithMessage(AccountHashedIdValidationMessage);
 
-        RuleFor(a => a.Ukprn)
-            .NotEmpty()
-            .When(a => a.Ukprn.HasValue)
-            .CheckUkprnFormat();
+        When(x => !string.IsNullOrWhiteSpace(x.AccountlegalentityPublicHashedId) || x.Ukprn.HasValue, () =>
+        {
+            RuleFor(x => x.AccountlegalentityPublicHashedId)
+                .NotEmpty()
+                .WithMessage(ConditionalParamsValidationMessage);
+
+            RuleFor(x => x.Ukprn)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(ConditionalParamsValidationMessage);
+
+            RuleFor(x => x.Ukprn)
+                .CheckUkprnFormat();
+        });
     }
 }
