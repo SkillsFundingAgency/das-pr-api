072aded4372f056b678ee5d197a2df4dc2968353
shaun-downey-education <Shaun.Downey@education.gov.uk>
2024-06-06 15:44:46 +0100

    CSP-1392: Remove duplicate file.

--- src/SFA.DAS.PR.Domain/Interfaces/IAccountProvidersReadRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.PR.Domain.Entities;
-
-namespace SFA.DAS.PR.Domain.Interfaces;
-
-public interface IAccountLegalEntityReadRepository
-{
-    Task<List<AccountLegalEntity>> GetAccountLegalEntiies(long accountId, CancellationToken cancellationToken);
-}
-
