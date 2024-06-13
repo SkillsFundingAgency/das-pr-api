404cfce4535231a4bd2d9a8325a0414384cc1cf7
Deven Shah <devenshah1974@gmail.com>
2024-06-13 12:29:18 +0100

    CSP-1479 Create JobAudit table

---
+++ src/SFA.DAS.PR.Database/Tables/JobAudit.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[JobAudit]
+(
+    [Id] BIGINT NOT NULL IDENTITY(1,1),
+    [JobName] NVARCHAR(100) NOT NULL, 
+    [JobInfo] NVARCHAR(MAX) NULL,
+    [ExecutedOn] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
+    CONSTRAINT JobAudit_PK PRIMARY KEY ([Id]),
+    INDEX JobAudit_IX ([JobName], [ExecutedOn])
+)
