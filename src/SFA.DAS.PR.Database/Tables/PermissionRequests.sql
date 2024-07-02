8e2d3eeb87eea251cce0200cf426728679d26179
Deven Shah <devenshah1974@gmail.com>
2024-07-02 16:10:09 +0100

    CSP-1465 Added request tables

---
+++ src/SFA.DAS.PR.Database/Tables/PermissionRequests.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PermissionRequests]
+(
+    [Id] uniqueidentifier default NEWID(),
+    [RequestId] uniqueidentifier NOT NULL,
+    [Operation] SMALLINT NOT NULL,
+    CONSTRAINT [PK_PermissionRequests] PRIMARY KEY ( [Id] )
+);
+GO
+
+CREATE INDEX [IX_PermissionRequests_RequestsId] ON [dbo].[PermissionRequests] (RequestId)
+INCLUDE (Operation);
+GO
---
+++ src/SFA.DAS.PR.Database/Tables/Requests.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[Requests]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+    [RequestType] VARCHAR(10) NOT NULL,
+    [Ukprn] BIGINT NOT NULL,
+    [RequestedBy] VARCHAR(255) NOT NULL,
+    [RequestedDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+    [AccountLegalEntityId] BIGINT NULL,
+    [EmployerOrganisationName] NVARCHAR(250) NULL,
+    [EmployerContactFirstName] NVARCHAR(200) NULL,
+    [EmployerContactLastName] NVARCHAR(200) NULL,
+    [EmployerContactEmail] NVARCHAR(255) NULL,
+    [EmployerPAYE] NVARCHAR(16) NULL,
+    [EmployerAORN] VARCHAR(25) NULL,
+    [Status] VARCHAR(10) NOT NULL DEFAULT 'New',
+    [ActionedBy] varchar(255) null,
+    [UpdatedDate] DATETIME2 NULL,
+    CONSTRAINT [PK_Requests] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE INDEX [IX_Requests_Ukprn] ON [dbo].[Requests]([Ukprn], [Status], [AccountLegalEntityId])
+INCLUDE ([Id], [RequestedDate]);
+GO
