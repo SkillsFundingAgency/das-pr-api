9c69c8d1ec6c3b9d1a857097c691bd0e84643667
shaun-downey-education <shaun.downey@education.gov.uk>
2024-07-02 23:18:18 +0100

    CSP-1465: Added required namespace.

--- src/SFA.DAS.PR.Data/EntityConfiguration/PermissionRequestConfiguration.cs
+++ src/SFA.DAS.PR.Data/EntityConfiguration/PermissionRequestConfiguration.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System.Diagnostics.CodeAnalysis;
+using SFA.DAS.PR.Domain.Entities;
 
 namespace SFA.DAS.PR.Data.EntityConfiguration;
 
