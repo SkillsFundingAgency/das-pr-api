249c60886625af7ffd1e66d91e7f63e7b2607999
Deven Shah <devenshah1974@gmail.com>
2024-07-09 17:30:30 +0100

    CSP-1431 Added view and test data script

---
+++ src/SFA.DAS.PR.Database/Views/ProviderRelationships.sql
@@ -0,0 +1,48 @@
+﻿CREATE VIEW [dbo].[ProviderRelationships]
+AS 
+
+SELECT 
+  apv.ProviderUkprn Ukprn, 
+  ale.Id AccountLegalEntityId, 
+  ale.PublicHashedId AgreementId, 
+  aple.Id AccountProviderLegalEntityId, 
+  ale.[Name] EmployerName, 
+  req.Id RequestId, 
+  CAST(per.Operation0 AS BIT) HasCreateCohortPermission, 
+  CAST(per.Operation1 AS BIT) HasCreateAdvertPermission, 
+  CAST(per.Operation2 AS BIT) HasCreateAdvertWithReviewPermission
+FROM [dbo].[AccountProviderLegalEntities] aple
+  JOIN [dbo].[AccountLegalEntities] ale on ale.[Id] = aple.[AccountLegalEntityId]
+  JOIN [dbo].[AccountProviders] apv on apv.[Id] = aple.[AccountProviderId]
+  LEFT JOIN [dbo].[Requests] req 
+      on ale.Id = req.AccountLegalEntityId 
+      AND req.Ukprn = apv.ProviderUkprn 
+      AND req.[Status] IN ('New','Sent')
+  LEFT JOIN (
+      SELECT [AccountProviderLegalEntityId],
+        MAX(CASE WHEN [operation] = 0 THEN 1 ELSE 0 END) Operation0,
+        MAX(CASE WHEN [operation] = 1 THEN 1 ELSE 0 END) Operation1,
+        MAX(CASE WHEN [operation] = 2 THEN 1 ELSE 0 END) Operation2
+      FROM [dbo].[Permissions] 
+      GROUP BY [AccountProviderLegalEntityId]
+      ) per on aple.Id = per.AccountProviderLegalEntityId
+
+UNION ALL
+
+SELECT 
+  req.Ukprn Ukprn, 
+  req.[AccountLegalEntityId] AccountLegalEntityId, 
+  null AgreementId, 
+  null AccountProviderLegalEntityId, 
+  ISNULL(req.EmployerOrganisationName, ale.Name) EmployerName, 
+  req.Id RequestId,  
+  null HasCreateCohortPermission, 
+  null HasCreateAdvertPermission, 
+  null HasCreateAdvertWithReviewPermission
+FROM [dbo].[Requests] req
+  LEFT JOIN [dbo].[AccountProviders] apv on req.Ukprn = apv.ProviderUkprn
+  LEFT JOIN [dbo].[AccountLegalEntities] ale on ale.[Id] = req.[AccountLegalEntityId]
+WHERE req.RequestType IN ('AddAccount','CreateAccount') 
+  AND req.[Status] IN ('New','Sent')
+
+GO
