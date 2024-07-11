e0991a9d9b083fe4dd59c759b7f8065500a9ce9f
Deven Shah <devenshah1974@gmail.com>
2024-07-11 13:21:43 +0100

    CSP-1431 Added get provider relationships endpoint

---
+++ src/SFA.DAS.PR.Api/Controllers/ProviderRelationshipsController.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.PR.Api.Authorization;
+using SFA.DAS.PR.Api.Common;
+using SFA.DAS.PR.Application.ProviderRelationships.Queries.GetProviderRelationships;
+using SFA.DAS.PR.Domain.QueryFilters;
+
+namespace SFA.DAS.PR.Api.Controllers;
+
+[Route("relationships/providers")]
+[ApiController]
+public class ProviderRelationshipsController(IMediator _mediator) : ActionResponseControllerBase
+{
+    public override string ControllerName => "ProviderRelationships";
+
+    [HttpGet("{ukprn}")]
+    [Authorize(Policy = Policies.Management)]
+    [ProducesResponseType(typeof(GetProviderRelationshipsQueryResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(List<ValidationError>), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetProviderRelationships([FromRoute] long ukprn, [FromQuery] ProviderRelationshipsRequestModel filters, CancellationToken cancellationToken)
+    {
+        GetProviderRelationshipsQuery query = GetQuery(ukprn, filters);
+        query.Ukprn = ukprn;
+        var result = await _mediator.Send(query, cancellationToken);
+        return GetResponse(result);
+    }
+
+    private GetProviderRelationshipsQuery GetQuery(long ukprn, ProviderRelationshipsRequestModel filters)
+        => new()
+        {
+            Ukprn = ukprn,
+            EmployerName = filters.EmployerName,
+            HasCreateAdvertPermission = filters.HasCreateAdvertPermission,
+            HasCreateAdvertWithReviewPermission = filters.HasCreateAdvertWithReviewPermission,
+            HasCreateCohortPermission = filters.HasCreateCohortPermission,
+            HasPendingRequest = filters.HasPendingRequest,
+            PageNumber = filters.PageNumber,
+            PageSize = filters.PageSize,
+        };
+}
---
+++ src/SFA.DAS.PR.Api/Models/ProviderRelationshipsRequestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.PR.Domain.QueryFilters;
+
+public class ProviderRelationshipsRequestModel
+{
+    public string? EmployerName { get; set; }
+    public bool HasPendingRequest { get; set; }
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+    public bool? HasCreateAdvertWithReviewPermission { get; set; }
+    public bool? HasCreateAdvertPermission { get; set; }
+    public bool? HasCreateCohortPermission { get; set; }
+}
