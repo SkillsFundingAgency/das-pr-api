9828743edf241c946e52897e8a2f4180f61b642e
Deven Shah <devenshah1974@gmail.com>
2024-07-11 09:23:29 +0100

    CSP-1431 Added query and query result

---
+++ src/SFA.DAS.PR.Application/ProviderRelationships/Queries/GetProviderRelationships/GetProviderRelationshipsQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using SFA.DAS.PR.Application.Mediatr.Responses;
+using SFA.DAS.PR.Domain.Interfaces;
+
+namespace SFA.DAS.PR.Application.ProviderRelationships.Queries.GetProviderRelationships;
+
+public class GetProviderRelationshipsQuery : IUkprnEntity, IRequest<ValidatedResponse<GetProviderRelationshipsQueryResult>>
+{
+    public long? Ukprn { get; set; }
+    public string? EmployerName { get; set; }
+    public bool HasPendingRequest { get; set; }
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+    public bool? HasCreateAdvertWithReviewPermission { get; set; }
+    public bool? HasCreateAdvertPermission { get; set; }
+    public bool? HasCreateCohortPermission { get; set; }
+}
---
+++ src/SFA.DAS.PR.Application/ProviderRelationships/Queries/GetProviderRelationships/ProviderRelationshipModel.cs
@@ -0,0 +1,28 @@
+﻿using SFA.DAS.PR.Domain.Entities;
+
+namespace SFA.DAS.PR.Application.ProviderRelationships.Queries.GetProviderRelationships;
+
+public class ProviderRelationshipModel
+{
+    public long Ukprn { get; set; }
+    public long? AccountLegalEntityId { get; set; }
+    public string? AgreementId { get; set; }
+    public Guid? RequestId { get; set; }
+    public string EmployerName { get; set; } = null!;
+    public bool? HasCreateCohortPermission { get; set; }
+    public bool? HasCreateAdvertPermission { get; set; }
+    public bool? HasCreateAdvertWithReviewPermission { get; set; }
+
+    public static implicit operator ProviderRelationshipModel(ProviderRelationship source) =>
+        new()
+        {
+            Ukprn = source.Ukprn,
+            AccountLegalEntityId = source.AccountLegalEntityId,
+            AgreementId = source.AgreementId,
+            RequestId = source.RequestId,
+            EmployerName = source.EmployerName,
+            HasCreateAdvertPermission = source.HasCreateAdvertPermission,
+            HasCreateAdvertWithReviewPermission = source.HasCreateAdvertWithReviewPermission,
+            HasCreateCohortPermission = source.HasCreateCohortPermission
+        };
+}
