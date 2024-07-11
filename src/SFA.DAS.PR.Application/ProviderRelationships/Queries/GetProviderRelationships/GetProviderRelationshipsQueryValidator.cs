73bcd0f0cf0cd13d274d047a0bf7e8e9ed12349e
Deven Shah <devenshah1974@gmail.com>
2024-07-11 09:45:46 +0100

    CSP-1431 Added query validator

---
+++ src/SFA.DAS.PR.Application/ProviderRelationships/Queries/GetProviderRelationships/GetProviderRelationshipsQueryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SFA.DAS.PR.Application.Common.Validators;
+using SFA.DAS.PR.Domain.Interfaces;
+
+namespace SFA.DAS.PR.Application.ProviderRelationships.Queries.GetProviderRelationships;
+public class GetProviderRelationshipsQueryValidator : AbstractValidator<GetProviderRelationshipsQuery>
+{
+    public const string UkprnNotSuppliedValidationMessage = "A Ukprn needs to be supplied";
+    public GetProviderRelationshipsQueryValidator(IProviderReadRepository providerReadRepository)
+    {
+        RuleFor(x => x.Ukprn)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .WithMessage(UkprnNotSuppliedValidationMessage);
+
+        RuleFor(x => x.Ukprn)
+            .IsValidUkprn(providerReadRepository);
+    }
+}
