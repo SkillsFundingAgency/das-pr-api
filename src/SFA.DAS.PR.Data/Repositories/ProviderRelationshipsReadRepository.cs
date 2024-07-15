0976717389e269c1b03f25516b761b22a6257230
Deven Shah <devenshah1974@gmail.com>
2024-07-15 14:38:39 +0100

    CSP-1431 The filter between recruit options should use OR clause

--- src/SFA.DAS.PR.Data/Repositories/ProviderRelationshipsReadRepository.cs
+++ src/SFA.DAS.PR.Data/Repositories/ProviderRelationshipsReadRepository.cs
@@ -18,8 +18,16 @@ public class ProviderRelationshipsReadRepository(IProviderRelationshipsDataConte
         if (providerRelationshipsQueryOptions.HasPendingRequest) query = query.Where(p => p.RequestId != null);
 
         if (providerRelationshipsQueryOptions.HasCreateCohortPermission == true) query = query.Where(p => p.HasCreateCohortPermission == true);
-        if (providerRelationshipsQueryOptions.HasCreateAdvertPermission == true) query = query.Where(p => p.HasCreateAdvertPermission == true);
-        if (providerRelationshipsQueryOptions.HasCreateAdvertWithReviewPermission == true) query = query.Where(p => p.HasCreateAdvertWithReviewPermission == true);
+
+        if (providerRelationshipsQueryOptions.HasCreateAdvertPermission == true && providerRelationshipsQueryOptions.HasCreateAdvertWithReviewPermission == true)
+        {
+            query = query.Where(p => p.HasCreateAdvertPermission == true || p.HasCreateAdvertWithReviewPermission == true);
+        }
+        else
+        {
+            if (providerRelationshipsQueryOptions.HasCreateAdvertPermission == true) query = query.Where(p => p.HasCreateAdvertPermission == true);
+            if (providerRelationshipsQueryOptions.HasCreateAdvertWithReviewPermission == true) query = query.Where(p => p.HasCreateAdvertWithReviewPermission == true);
+        }
 
         var count = await query.CountAsync(cancellationToken);
 
