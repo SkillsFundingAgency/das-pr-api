920ef4358d0deb721e4399c51d416f9bd09aa09a
MarkFCain <mark.cain@digital.education.gov.uk>
2024-07-23 11:50:04 +0100

    CSP-1371 Get Permissions extended to return provider and ALE names

--- src/SFA.DAS.PR.Application/Permissions/Queries/GetPermissions/GetPermissionsQueryResult.cs
+++ src/SFA.DAS.PR.Application/Permissions/Queries/GetPermissions/GetPermissionsQueryResult.cs
@@ -4,4 +4,6 @@ namespace SFA.DAS.PR.Application.Permissions.Queries.GetPermissions;
 public class GetPermissionsQueryResult
 {
     public List<Operation> Operations { get; set; } = new();
+    public string ProviderName { get; set; } = null!;
+    public string AccountLegalEntityName { get; set; } = null!;
 }
