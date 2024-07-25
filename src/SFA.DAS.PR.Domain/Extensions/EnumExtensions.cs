c12e532e73cbe0f0acf4a7ddf9d8ca784a4ea828
shaun-downey-education <shaun.downey@education.gov.uk>
2024-07-25 10:10:27 +0100

    CSP-1517: Remove ToLowerString() extension (not required). Update email validation to throw an error on empty string with additional unit testing.

--- src/SFA.DAS.PR.Domain/Extensions/EnumExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.PR.Domain.Extensions;
-
-public static class EnumExtensions
-{
-    public static string ToLowerString(this Enum enumValue)
-    {
-        return enumValue.ToString().ToLower();
-    }
-}
