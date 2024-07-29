478334a1a52f89e47481e294269898789e0bba16
MarkFCain <mark.cain@digital.education.gov.uk>
2024-07-29 08:59:48 +0100

    CSP-1371 minor amend based on TR comment

--- src/SFA.DAS.PR.Application/Permissions/Queries/GetPermissions/GetPermissionsQueryResult.cs
+++ src/SFA.DAS.PR.Application/Permissions/Queries/GetPermissions/GetPermissionsQueryResult.cs
@@ -4,6 +4,6 @@ namespace SFA.DAS.PR.Application.Permissions.Queries.GetPermissions;
 public class GetPermissionsQueryResult
 {
     public List<Operation> Operations { get; set; } = new();
-    public string ProviderName { get; set; } = null!;
-    public string AccountLegalEntityName { get; set; } = null!;
+    public required string ProviderName { get; set; }
+    public required string AccountLegalEntityName { get; set; }
 }
