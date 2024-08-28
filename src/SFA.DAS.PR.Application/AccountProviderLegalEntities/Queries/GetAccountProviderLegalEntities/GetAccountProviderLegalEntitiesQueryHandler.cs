e4058d648cb51db11b9d19ff5cd9ec22c39d439d
Deven Shah <devenshah1974@gmail.com>
2024-08-28 18:27:06 +0100

    FAI-1889 Fix integration get account legal entities to use any recruit permission

--- src/SFA.DAS.PR.Application/AccountProviderLegalEntities/Queries/GetAccountProviderLegalEntities/GetAccountProviderLegalEntitiesQueryHandler.cs
+++ src/SFA.DAS.PR.Application/AccountProviderLegalEntities/Queries/GetAccountProviderLegalEntities/GetAccountProviderLegalEntitiesQueryHandler.cs
@@ -2,12 +2,18 @@
 using SFA.DAS.PR.Application.Mediatr.Responses;
 using SFA.DAS.PR.Domain.Entities;
 using SFA.DAS.PR.Domain.Interfaces;
+using SFA.DAS.ProviderRelationships.Types.Models;
 
 namespace SFA.DAS.PR.Application.AccountProviderLegalEntities.Queries.GetAccountProviderLegalEntities;
 public class GetAccountProviderLegalEntitiesQueryHandler(IAccountProviderLegalEntitiesReadRepository _accountProviderLegalEntitiesReadRepository) : IRequestHandler<GetAccountProviderLegalEntitiesQuery, ValidatedResponse<GetAccountProviderLegalEntitiesQueryResult>>
 {
     public async Task<ValidatedResponse<GetAccountProviderLegalEntitiesQueryResult>> Handle(GetAccountProviderLegalEntitiesQuery query, CancellationToken cancellationToken)
     {
+        if (query.Operations.Exists(o => o == Operation.RecruitmentRequiresReview))
+        {
+            query.Operations.Add(Operation.Recruitment);
+        }
+
         List<AccountProviderLegalEntity> accountProviderLegalEntities = await _accountProviderLegalEntitiesReadRepository.GetAccountProviderLegalEntities(query.Ukprn, query.AccountHashedId, query.Operations, cancellationToken);
 
         GetAccountProviderLegalEntitiesQueryResult result = new(accountProviderLegalEntities.Select(a => (AccountProviderLegalEntityModel)a).ToList());
