3d65ada17d117bae18ca797383a141257abffb4a
Peter Williams <peter.david.williams.1991@gmail.com>
2024-11-20 09:39:15 +0000

    CSP-1604 - Added missing coverage for new repository method

--- src/SFA.DAS.PR.Data.UnitTests/Repositories/EmployerRelationshipsReadRepositoryTests.cs
+++ src/SFA.DAS.PR.Data.UnitTests/Repositories/EmployerRelationshipsReadRepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using AutoFixture.NUnit3;
+using NUnit.Framework;
 using SFA.DAS.PR.Data.Repositories;
 using SFA.DAS.PR.Data.UnitTests.InMemoryDatabases;
 using SFA.DAS.PR.Data.UnitTests.Setup;
@@ -20,7 +21,7 @@ public class EmployerRelationshipsReadRepositoryTests
 
         Account? result = new();
 
-        using (var context = InMemoryProviderRelationshipsDataContext.CreateInMemoryContext(
+        await using (var context = InMemoryProviderRelationshipsDataContext.CreateInMemoryContext(
             $"{nameof(InMemoryProviderRelationshipsDataContext)}_{nameof(GetRelationships_Returns_Permissions)}")
         )
         {
@@ -49,5 +50,29 @@ public class EmployerRelationshipsReadRepositoryTests
                            .ToList(),
                         Has.Count.EqualTo(1), "Requests count should be 1");
         });
+    }
+
+    [Test]
+    [InlineAutoData(123456, 123456, true)]
+    [InlineAutoData(654321, 1234567, false)]
+    public async Task AccountIdExists_Returns_Expected(long accountIdToCheck, long accountIdExisting, bool expected)
+    {
+        Account account = AccountTestData.CreateAccount(accountIdExisting);
+
+        bool result;
+
+        await using (var context = InMemoryProviderRelationshipsDataContext.CreateInMemoryContext(
+                   $"{nameof(InMemoryProviderRelationshipsDataContext)}_{nameof(AccountIdExists_Returns_Expected)}")
+              )
+        {
+            await context.AddAsync(account);
+            await context.SaveChangesAsync(cancellationToken);
+
+            EmployerRelationshipsReadRepository sut = new(context);
+
+            result = await sut.AccountIdExists(accountIdToCheck, cancellationToken);
+        }
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
