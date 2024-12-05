5d945ccdcf7f87613116e500c8a5e260bbfb4292
MarkFCain <mark.cain@digital.education.gov.uk>
2024-12-05 13:54:04 +0000

    CSP-1772 paye added to POST addAccount

--- src/SFA.DAS.PR.Application/Requests/Commands/CreateAddAccountRequest/CreateAddAccountRequestCommand.cs
+++ src/SFA.DAS.PR.Application/Requests/Commands/CreateAddAccountRequest/CreateAddAccountRequestCommand.cs
@@ -8,12 +8,13 @@ namespace SFA.DAS.PR.Application.Requests.Commands.CreateAddAccountRequest;
 public class CreateAddAccountRequestCommand : IRequest<ValidatedResponse<CreateAddAccountRequestCommandResult>>, IUkprnEntity, IOperationsEntity, IAccountLegalEntityIdEntity
 {
     public required long? Ukprn { get; set; }
-    
+
     public required string RequestedBy { get; set; }
 
     public required long AccountLegalEntityId { get; set; }
 
     public string? EmployerContactEmail { get; set; }
+    public string? Paye { get; set; }
 
     public required List<Operation> Operations { get; set; } = [];
 }
